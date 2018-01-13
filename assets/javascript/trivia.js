f32d88ff322628654ba41008d478137ac181ad4a
erin hubbard <erinhubbard1981@gmail.com>
2018-01-13 09:39:32 -0500

    all functioning so far

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -3,12 +3,19 @@
 //log button click
 //determine if correct
 //stop timer and disply results on click of submit
-$(document).ready(function() {
+window.onload = function() {
 
 var number = 45;
 var intervalId;
 var correct = 0;
 var incorrect = 0;
+var timerRunning = false;
+var score;
+
+//var modal = $("#myModal");
+//var button = $("#button");
+//var span = $(".close")
+
 
 function startGame() {
 	intervalId = setInterval(decrement, 1000);
@@ -16,7 +23,7 @@ function startGame() {
 
 function decrement() {
 	number--;
-	$("#timer").html("Time Left: " + number);
+	$("#timer").html("Time Left: " + number + " seconds");
 	console.log("start");
 
 	if (number === 0) {
@@ -29,6 +36,41 @@ function stop() {
 	clearInterval(intervalId);
 }
 
-
-
 startGame();
+
+$(".correct").on("click",function() {
+	correct++;
+	console.log("clicked right");
+});
+
+$(".incorrect").on("click",function() {
+	incorrect++;
+	console.log("clicked wrong");
+});
+
+$("#button").on("click",function() {
+	stop();
+	console.log("button works");
+
+	if (incorrect <= 2 && correct >= 3) {
+		$("#game").html("You won!");
+
+	}else {
+		$("#game").html("You lost!");
+
+	};
+
+  });
+
+};
+
+/*$("#playAgain").on("click", function(){
+		startGame();
+		console.log("button clicked");
+});
+*/
+	//$("#modalContent").modal();
+
+
+//$("#myModal").html("Correct: " + correct);
+//$("#myModal").html("Incorrect: " + incorrect);
